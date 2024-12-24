86b42b2c7e64c7d0fa11b3c7d2522b4c41651d46
slavarodionov <roval911@gmail.com>
2024-12-24 17:05:59 +0300

    config

--- cmd/main.go
+++ cmd/main.go
@@ -3,31 +3,57 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"gw-currency-wallet/internal/config"
 	"gw-currency-wallet/internal/hanlers"
 	"gw-currency-wallet/internal/repository"
 	"log"
 	"os"
 )
 
-func init() {
-	// Загружаем переменные окружения из .env файла
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-	// Инициализация базы данных
-	repository.InitDb()
-
-	// Запуск миграций
-	repository.RunMigrations()
-}
+const (
+	CONFIG_DIR  = "configs"
+	CONFIG_FILE = "main"
+)
 
 func main() {
+	cfg, err := config.New(CONFIG_DIR, CONFIG_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("config: %+v\n", cfg)
+
+	// init db
+	db, err := repository.NewPostgresConnection(repository.ConnectionInfo{
+		Host:     cfg.DB.Host,
+		Port:     cfg.DB.Port,
+		Username: cfg.DB.Username,
+		DBName:   cfg.DB.Name,
+		SSLMode:  cfg.DB.SSLMode,
+		Password: cfg.DB.Password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Устанавливаем db в глобальную переменную repository
+	repository.SetDB(db)
+
+	// Запускаем миграции
+	repository.RunMigrations()
+
+	// Инициализируем сервер
 	router := gin.Default()
 	router.Use(cors.Default())
 
 	//router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.POST("/api/v1/register", hanlers.CreateUserHandler)
 
-	router.Run(os.Getenv("PORT"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Дефолтный порт
+	}
+
+	router.Run(port)
 }
--- internal/repository/conector.go
+++ internal/repository/conector.go
@@ -8,20 +8,31 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"log"
-	"os"
 )
 
 var db *sql.DB
 
-func InitDb() {
-	var err error
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
-	db, err = sql.Open("postgres", connStr)
+type ConnectionInfo struct {
+	Host     string
+	Port     int
+	Username string
+	DBName   string
+	SSLMode  string
+	Password string
+}
+
+func NewPostgresConnection(info ConnectionInfo) (*sql.DB, error) {
+	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=%s password=%s",
+		info.Host, info.Port, info.Username, info.DBName, info.SSLMode, info.Password))
 	if err != nil {
-		log.Fatal("Error connecting to the database: ", err)
+		return nil, err
 	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 // Закрытие соединения с базой данных
